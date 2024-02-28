1ea5254fe6d034f2465b0beaa61e8d991694715c
Xenock20 <juliosaboredo@gmail.com>
2024-02-28 14:59:18 -0300

    Fix websocket files

--- server/index.php
+++ server/index.php
@@ -1,3 +1,37 @@
 <?php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
 
-phpinfo();
+class MyWebSocketServer implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn) {
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        echo "Message from {$from->resourceId}: $msg\n";
+        // Aquí puedes procesar el mensaje recibido y enviar una respuesta si es necesario.
+        $from->send("Received: $msg");
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+}
+
+// Iniciar el servidor
+$server = IoServer::factory(
+    new WsServer(new MyWebSocketServer()),
+    8080 // Puerto en el que escuchará el servidor
+);
+
+echo "WebSocket server started\n";
+
+// Ejecutar el servidor
+$server->run();
